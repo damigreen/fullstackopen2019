daa802869b55328d25bd61942d81f4e03b2eeb7f
Damilola Faseun <fashfired@gmail.com>
2020-07-15 19:41:39 +0100

    Added a baseUrl to connect to server

--- part7/bloglist-frontend/src/services/login.js
+++ part7/bloglist-frontend/src/services/login.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = '/api/login';
-console.log(baseUrl);
-
+const baseUrl = 'http://localhost:3003/api/login';
 
 const login = async (credentials) => {
   const response = await axios({

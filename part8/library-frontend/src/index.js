cb7bb556bde2a61e3b13dd877c5b618d10f17fb4
Damilola Faseun <fashfired@gmail.com>
2020-08-22 07:11:14 +0100

    Connect Apollo client to App and wrap the ApolloProvider

---
+++ part8/library-frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
+
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'))

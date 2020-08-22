8b83a7a412e05284f693d20c9041f5e7644b21fb
Damilola Faseun <fashfired@gmail.com>
2020-08-22 07:12:43 +0100

    Retrive data fromt the App for the Author component

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null
+  }
+
+  if (props.result.loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+  const authors = props.result.data.allAuthors
+
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors

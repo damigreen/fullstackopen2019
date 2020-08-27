bc3abf3ea69ba490f9b84d7a829ac68cc16b8874
Damilola Faseun <fashfired@gmail.com>
2020-08-27 11:10:23 +0100

    Added a user model to mongoDB

---
+++ part8/book-archive/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  favoriteGenre: {
+    type: String,
+    required: true, 
+  }
+})
+
+module.exports = mongoose.model('User', schema);

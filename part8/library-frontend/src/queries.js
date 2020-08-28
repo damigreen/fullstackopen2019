1f03cf3c4a6f86e9e6bbe047051384c3758386c6
Damilola Faseun <fashfired@gmail.com>
2020-08-28 12:01:50 +0100

    Added a new modules for queries

---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,51 @@
+import { gql } from 'apollo-boost'
+
+
+export const ALL_AUTHORS = gql`
+{
+  allAuthors {
+    name
+    born
+    bookCount
+    id
+  }
+}
+`
+export const ALL_BOOKS = gql`
+{
+  allBooks {
+    title
+    published
+    author {
+      name
+    }
+    genres
+  }
+}
+`
+export const ADD_BOOK = gql`
+mutation createBook($title: String!, $published: Int!, $author: String! $genres: [String!]!) {
+  addBook(
+    title: $title,
+    published: $published,
+    author: $author,
+    genres: $genres
+  ) {
+    title
+    published
+    author {
+      name
+    }
+    genres
+    id
+  }
+}
+`
+export const EDIT_AUTHOR = gql`
+mutation authorEdit($name: String!, $setBornTo: Int!) {
+  editAuthor(name: $name, setBornTo: $setBornTo) {
+    name
+    born
+  }
+}
+`

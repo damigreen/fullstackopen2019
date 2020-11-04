dd98fd0b0a915fea057e618c9c1de0123c1a908f
Damilola Faseun <fashfired@gmail.com>
2020-11-04 15:29:34 +0100

    Created a service to get all diagnoses entry and add

--- part9/patientor app/backend/src/services/diagnosesServices.ts
+++ part9/patientor app/backend/src/services/diagnosesServices.ts
@@ -1,1 +1,19 @@
-import express from 'express';
+// import express from 'express';
+
+import diagnosesData from '../../data/diagnoses.json';
+import { DiagnosesEntry } from '../types';
+
+const diagnoses: Array<DiagnosesEntry> = diagnosesData;
+
+const getDiagnosesEntries = (): Array<DiagnosesEntry> => {
+  return diagnoses;
+};
+
+const addDiagnosesEntry = () => {
+  return null;
+};
+
+export default {
+  getDiagnosesEntries,
+  addDiagnosesEntry,
+};

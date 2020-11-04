4622f021b3afdcf6be3476318a7c00292a4e41ea
Damilola Faseun <fashfired@gmail.com>
2020-11-04 17:46:26 +0100

    Use cors and route in app

--- part9/patientor app/backend/src/index.ts
+++ part9/patientor app/backend/src/index.ts
@@ -1,19 +1,21 @@
 import express from 'express';
 import diagnosesRouter from './routes/diagnoses';
 import patientRouter from './routes/patients'
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.static('build'));
+app.use(cors());
 
 app.get('/ping', (_req, res) => {
   res.send('PONG----------------------------------');
 });
 
-app.use('/diagnoses', diagnosesRouter);
-app.use('/patients', patientRouter);
+app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientRouter);
 
-const PORT = 3002;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log("God is great");

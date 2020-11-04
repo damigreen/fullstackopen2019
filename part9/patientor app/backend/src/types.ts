d44f433a709487c5333934e54296b4ced9d81ff1
Damilola Faseun <fashfired@gmail.com>
2020-11-04 16:48:07 +0100

    Added types for patients

--- part9/patientor app/backend/src/types.ts
+++ part9/patientor app/backend/src/types.ts
@@ -3,5 +3,16 @@
 export interface DiagnosesEntry {
   code: string;
   name: string;
-  latin: string;
+  latin?: string;
 };
+
+export interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+};
+
+export type NonSensitiveDiagnosesEntry  = Omit<DiagnosesEntry, 'latin'>;

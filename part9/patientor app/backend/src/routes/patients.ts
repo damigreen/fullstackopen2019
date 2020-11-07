755164c1b7024a213f6b1be67f59731df8fef3b7
Damilola Faseun <fashfired@gmail.com>
2020-11-07 08:36:47 +0100

    Created a route to post new patients

--- part9/patientor app/backend/src/routes/patients.ts
+++ part9/patientor app/backend/src/routes/patients.ts
@@ -9,4 +9,14 @@ patientRouter.get('/', (_req, res) => {
   res.send(patientServices.getNonSensitivePatientsEntry());
 });
 
+patientRouter.post('/', (req, res) => {
+  try {
+    const newPatientEntry = patientServices.addPatient(req.body)
+    res.json(newPatientEntry);
+  } catch (e) {
+    // console.log(object)
+    res.status(400).send(e.message);
+  }
+});
+
 export default patientRouter;

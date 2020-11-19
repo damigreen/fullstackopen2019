7a05d35daff7d6d96e87c432cdb2dfd36bac3db4
Damilola Faseun <fashfired@gmail.com>
2020-11-19 11:57:55 +0100

    Displayed the initial application data

---
+++ part9/course-content/src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  const courseName = "Half stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    },
+  ];
+
+  return (
+    <div className="App">
+      <h1>{courseName}</h1>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+      <p>
+        Number of exercise{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  );
+}
+
+export default App;

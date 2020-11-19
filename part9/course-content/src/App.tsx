01fbdf9a4dd7e974e0420f394d01be9fa0fb6846
Damilola Faseun <fashfired@gmail.com>
2020-11-19 13:53:58 +0100

    Refactored app component using seerate component

--- part9/course-content/src/App.tsx
+++ part9/course-content/src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
+import Content from './components/Content';
+import Total from './components/Total';
 import './App.css';
+
 
 const App: React.FC = () => {
   const courseName = "Half stack application development";
@@ -20,20 +24,10 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <h1>{courseName}</h1>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-      <p>
-        Number of exercise{" "}
-        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
-      </p>
+      <Header name={courseName} />
+      <Content content={courseParts} />
+      <Total total={courseParts} />
+
     </div>
   );
 }

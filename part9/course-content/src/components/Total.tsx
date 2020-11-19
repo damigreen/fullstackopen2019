968cc5f48cfd9bc2596830f6f1a916e886577281
Damilola Faseun <fashfired@gmail.com>
2020-11-19 13:55:39 +0100

    Created a component to calculate the total scores

--- part9/course-content/src/components/Total.tsx
+++ part9/course-content/src/components/Total.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Total: React.FC = () => {
+const Total: React.FC<{ total: { name: string, exerciseCount: number }[] }> = ({ total }) => {
 
+  const total_scores = total.reduce((acc, curr) => acc + curr.exerciseCount, 0)
+
+  console.log(total_scores);
   return (
     <div>
-      <h1>Total Component</h1>
+      <h1>Total {total_scores}</h1>
+      
     </div>
   )
 }
